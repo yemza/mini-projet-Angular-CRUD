91e262b367fd1d55a5b9084f1b1b9be08b42294b
yemza <elabouazzaoui729@gmail.com>
2023-01-24 18:10:47 +0100

    Ajust Code

--- src/main/java/com/miniprojetAngularCRUD/Controlers/UserControler.java
+++ src/main/java/com/miniprojetAngularCRUD/Controlers/UserControler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.miniprojetAngularCRUD.Entities.Utilisateur;
@@ -20,21 +21,26 @@ public class UserControler {
 	UtilisateurService	utilisateurService;
 	
 	@PostMapping("/addUser")
-	public Utilisateur addUserOrUpdate(Utilisateur utilisateur) {
+	public Utilisateur addUser(@RequestBody Utilisateur utilisateur) {
 		return utilisateurService.addUserOrUpdate(utilisateur);
 	}
 
+	@PostMapping("/updateUser")
+	public Utilisateur UpdateUser(@RequestBody Utilisateur utilisateur) {
+		return utilisateurService.addUserOrUpdate(utilisateur);
+	}
+	
 	@DeleteMapping("/removeUser")
 	public void removeUser(Long idUtilisateur) {
 		utilisateurService.removeUser(idUtilisateur);	
 	}
 
-	@GetMapping("/GetUser")
+	@GetMapping("/getUser")
 	public Utilisateur getUser(Long idUtilisateur) {
 		return utilisateurService.getUser(idUtilisateur);	
 	}
 
-	@GetMapping("/GetUsers")
+	@GetMapping("/getUsers")
 	public List<Utilisateur> getUsers() {
 		return utilisateurService.getUsers();	
 	}
--- src/main/java/com/miniprojetAngularCRUD/Services/UtilisateurServiceImpl.java
+++ src/main/java/com/miniprojetAngularCRUD/Services/UtilisateurServiceImpl.java
@@ -3,10 +3,12 @@ package com.miniprojetAngularCRUD.Services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.miniprojetAngularCRUD.Entities.Utilisateur;
 import com.miniprojetAngularCRUD.Respository.UtilisateurRespository;
 
+@Service
 public class UtilisateurServiceImpl implements UtilisateurService {
 	
 	@Autowired
