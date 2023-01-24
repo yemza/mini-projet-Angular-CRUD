905e48f890b098864aa871a94e79fe5d7cfc8df9
yemza <elabouazzaoui729@gmail.com>
2023-01-24 15:56:00 +0100

    Add User Controller

---
+++ src/main/java/com/miniprojetAngularCRUD/Controlers/UserControler.java
@@ -0,0 +1,42 @@
+package com.miniprojetAngularCRUD.Controlers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.miniprojetAngularCRUD.Entities.Utilisateur;
+import com.miniprojetAngularCRUD.Services.UtilisateurService;
+
+@CrossOrigin("*")
+@RestController
+public class UserControler {
+
+	@Autowired
+	UtilisateurService	utilisateurService;
+	
+	@PostMapping("/addUser")
+	public Utilisateur addUserOrUpdate(Utilisateur utilisateur) {
+		return utilisateurService.addUserOrUpdate(utilisateur);
+	}
+
+	@DeleteMapping("/removeUser")
+	public void removeUser(Long idUtilisateur) {
+		utilisateurService.removeUser(idUtilisateur);	
+	}
+
+	@GetMapping("/GetUser")
+	public Utilisateur getUser(Long idUtilisateur) {
+		return utilisateurService.getUser(idUtilisateur);	
+	}
+
+	@GetMapping("/GetUsers")
+	public List<Utilisateur> getUsers() {
+		return utilisateurService.getUsers();	
+	}
+
+}
