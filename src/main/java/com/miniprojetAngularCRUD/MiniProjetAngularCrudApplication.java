485f4235d69c585989c2873a5e4f6d981bc9a49d
yemza <elabouazzaoui729@gmail.com>
2023-01-24 14:41:09 +0100

    first commit

---
+++ src/main/java/com/miniprojetAngularCRUD/MiniProjetAngularCrudApplication.java
@@ -0,0 +1,13 @@
+package com.miniprojetAngularCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjetAngularCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjetAngularCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miniprojetAngularCRUD/MiniProjetAngularCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniprojetAngularCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjetAngularCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
